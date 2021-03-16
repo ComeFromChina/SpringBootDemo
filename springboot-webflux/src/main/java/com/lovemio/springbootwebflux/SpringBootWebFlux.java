df63e7e93b228012306982d3b088649d5bc72c9f
liwenjin <455193135@qq.com>
2021-03-16 18:03:06 +0800

    SpringBoot-webFlux learn

---
+++ springboot-webflux/src/main/java/com/lovemio/springbootwebflux/SpringBootWebFlux.java
@@ -0,0 +1,13 @@
+package com.lovemio.springbootwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebFlux {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebFlux.class, args);
+    }
+
+}
---
+++ springboot-webflux/src/main/java/com/lovemio/springbootwebflux/entity/User.java
@@ -0,0 +1,16 @@
+package com.lovemio.springbootwebflux.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    private Long id;
+    String username;
+    String nickname;
+}
