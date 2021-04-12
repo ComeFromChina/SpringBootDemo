237d2fb9b802fd6179e3c197ca2fbaa20a1de34d
liwenjin <455193135@qq.com>
2021-04-12 18:23:42 +0800

    SpringBoot Kinfe4j 集成、Spring Validation 参数校验测试

---
+++ springboot-swagger-knife4j/src/main/java/com/lovemio/knife4j/SpringbootSwaggerKnife4jApplication.java
@@ -0,0 +1,13 @@
+package com.lovemio.knife4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwaggerKnife4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwaggerKnife4jApplication.class, args);
+    }
+
+}
---
+++ springboot-swagger-knife4j/src/test/java/com/lovemio/knife4j/SpringbootSwaggerKnife4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovemio.knife4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerKnife4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
