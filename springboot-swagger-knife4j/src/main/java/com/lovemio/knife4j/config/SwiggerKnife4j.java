1f3591f0e907df5d2091ef4cb421485887df1c7d
liwenjin <455193135@qq.com>
2023-12-01 16:32:48 +0800

    SpringBoot 集成mybatis-plus

--- springboot-swagger-knife4j/src/main/java/com/lovemio/knife4j/config/SwiggerKnife4j.java
+++ springboot-swagger-knife4j/src/main/java/com/lovemio/knife4j/config/SwiggerKnife4j.java
@@ -3,14 +3,20 @@ package com.lovemio.knife4j.config;
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author liwenjin
@@ -42,5 +48,12 @@ public class SwiggerKnife4j {
                 .version("1.1.0")//版本号
                 .build();
     }
+    /**
+     * 安全模式，这里配置通过请求头 Authorization 传递 token 参数
+     */
+    private static List<SecurityScheme> securitySchemes() {
+        return Collections.singletonList(new ApiKey(HttpHeaders.AUTHORIZATION, "Authorization", "header"));
+    }
+
 }
 
